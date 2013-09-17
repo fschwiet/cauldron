88f5d064b207be82fb626bc1ec20e0251dd648e1
fschwiet <fschwiet@gmail.com>
2013-09-17 10:59:43 -0700

    path fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
     assertPage: require("./lib/assertPage.js"),
     phantom: require("./lib/node-phantom-shim.js"),
     nodeunit: require("./lib/nodeunit-builder.js"),
-    shouldFail: require("./lib/shouldFail.js"),
+    shouldFail: require("./lib/should-fail.js"),
     usingPhantomPage: require("./lib/using-phantom-page.js"),
     waitUntil: require("./lib/waitUntil.js")
 };
